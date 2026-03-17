ec7bcfb37d6e950faf1ccd307ae67f80d32ca42c
Anslie Pettit <apettit1112@gmail.com>
2026-03-17 09:47:58 -0700

    Step 4: Users Table setup changes

--- database/setup.js
+++ database/setup.js
@@ -8,6 +8,27 @@ const db = new Sequelize({
   logging: console.log
 });
 
+// Define User model
+const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
 // Define Project model
 const Project = db.define('Project', {
     id: {
